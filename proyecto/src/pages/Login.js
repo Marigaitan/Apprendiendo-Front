09edc2a7b68fc721f8313a1211388061e7cd06df
Marigaitan <marielgaitan93@gmail.com>
2021-08-18 21:43:17 -0300

    Commiteo

---
+++ proyecto/src/pages/Login.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react'
+import im from '../Images/logo-verde.png'
+import '../css/Login.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios' //para las peticiones a la API
+//import md5 from 'md5'
+import Cookies from 'universal-cookie/es6'
+
+const baseUrl = "http://localhost:8080/login";  //Url de la API
+const cookies = new Cookies();
+
+export default class Login extends Component {
+
+    //Necesitamos un estado y un metodo para capturar lo que el usuario ingrese
+    state = {
+        form: {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleChange = async e => {   //con este metodo guardamos en el estado el valor del imput de acuerdo 
+        await this.setState({     //a su nombre. Asi que los estado tienen que coincidir con el nombre
+            form: {              //de los imputs.
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+        //console.log(this.state.form); //Lo mostramos por consola para ver que funcione
+    }                              //uso async y await para q sea asincrono y verlo en tiempo real
+    
+    
+    sendCredentials = async () => {
+        let body = { username: this.state.form.username, password: this.state.form.password };
+        await axios.post(baseUrl, body) //aca podria usar MD5
+            .then(response => {
+                cookies.set('token', response.headers.Authentication, { path: "/" });
+                return response.data; //aca retonamos la data que viene en el body
+            })
+            .then(response => {  //aca la podemos utilizar
+                if (response.length > 0) {
+                    var respuesta = response[0];
+                    cookies.set('username', respuesta.username, { path: "/" });
+                    cookies.set('role', respuesta.role, { path: "/" });
+                    //ver como lo saco de la cabecera buscar axios.header
+                    debugger
+                    if (cookies.get('role') === "ROLE_STUDENT") {
+                        window.location.href = "./menualumno";
+                    } else if (cookies.get('role') === "ROLE_TEACHER") {
+                        window.location.href = "./menudocente";
+                    } else if (cookies.get('role') === "ROLE_ADMIN") {
+                        window.location.href = "./menuadmin";
+                    }
+
+
+                } else {
+                    alert('El usuario o la contraseña no son correctos');
+                }
+
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        return (
+            <div className="containerPrincipal">
+                <div className="containerSecundario">
+                    <img
+                        src={im}
+                        id="logo"
+                        alt="No se encuentra la imagen" />
+                    <br />
+                    <input
+                        type="user"
+                        className="inputs"
+                        name="username"
+                        placeholder="Usuario"
+                        maxLength="10"
+                        onChange={this.handleChange} />
+                    <br />
+                    <input
+                        type="password"
+                        className="inputs"
+                        name="password"
+                        placeholder="Contraseña"
+                        maxLength="10"
+                        onChange={this.handleChange} />
+                    <br />
+                    <button onClick={() => this.sendCredentials()} className="submit-button">Ingresar</button>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ proyecto/src/pages/MenuAlumno.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import Cookies from 'universal-cookie/es6'
+import img from '../Images/logoMini.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap';
+
+
+const cookies = new Cookies();
+
+export default class MenuAlumno extends Component {
+    constructor(props) {        //constructor de mi clase
+        super(props);
+        this.state = {dropdown: false};
+      }
+    cerrarSesion=()=>{
+        cookies.remove('token', {path: "/"});
+        cookies.remove('username', {path: "/"});
+        cookies.remove('role', {path: "/"});
+        window.location.href='./' //lo redirijo al login
+    }
+
+    componentDidMount(){    //para que lo redirija al login si no hay token
+        if(!cookies.get('token')){
+            window.location.href="./";
+        }
+    }
+
+    Menu() {
+        const opencloseDropdown=()=>{
+            this.setState({dropdown:!this.state.dropdown});
+        } 
+        const irPerfil=()=>{
+            alert("aca se ve el perfil de usuario");
+        }
+        
+
+        return (
+            <div className = 'DropMenu'>
+                <Dropdown isOpen={this.state.dropdown} toggle={opencloseDropdown}>
+                    <DropdownToggle caret> 
+                     Dropdown Ejemplo    {/*boton que se va a desplegar */}
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem onClick={()=>irPerfil}>Ver Perfil</DropdownItem>
+                        <DropdownItem divider/>
+                        <DropdownItem onClick={()=>this.cerrarSesion()}>Cerrar Sesión</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
+            </div>
+        )
+        
+    }
+    render() {
+        console.log('role: ' + cookies.get('role'));
+        console.log('username: ' + cookies.get('username'));
+        return (
+            <div>
+                Menú principal del alumno
+                <img 
+                    src= {img}
+                    id="logoAvatar"
+                    alt= "No se encuentra la imagen"
+                    />
+                <h2>Hola {cookies.get('username')}</h2>
+                <br />
+                <button onClick={()=> this.cerrarSesion()}>cerrar sesión</button> {/* Provisorio hasta tener el menu desplegable */}
+            </div>
+        )
+    }
+}
