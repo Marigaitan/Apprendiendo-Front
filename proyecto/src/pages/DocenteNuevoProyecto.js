298d1ec45a0f935e0124e258a85e6585110e5f20
Javier Leonardo Soto <jlsoto85@gmail.com>
2021-08-24 15:24:04 -0300

    avance con DocenteNuevoProyecto

--- proyecto/src/pages/DocenteNuevoProyecto.js
+++ proyecto/src/pages/DocenteNuevoProyecto.js
@@ -5,20 +5,23 @@ import Header from './Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import img from '../Images/account.png'
 import '../css/MenuAlumno.css'
+import { get } from 'lodash';
 
 const cookies = new Cookies();
 let newProjectUrl = "http://localhost:8080/classroom/" + cookies.get('classId') + "/project";
+let getMethodologiesUrl = "http://localhost:8080/methodologies";
 
 export default class DocenteNuevoProyecto extends Component {
     constructor(props) {        //constructor de mi clase
         super(props);
-        this.state = { subject: "", year: 0, division: "", teacherId: -1, students: [], projects: [], teacherName: "" };
+        this.state = {name: "Nuevo Proyecto", teacherId: -1, methodologies: [], methodologyId: -1};
     }
-    //Necesitamos un estado y un metodo para capturar lo que el usuario ingrese
+    
+    
+    //TODO: en lugar de username y password, seria nombre del proyecto y seleccionar una metodologia de la lista que devuelve this.getMethodologies()
     state = {
         form: {
-            username: '',
-            password: ''
+            name: ''
         }
     }
 
@@ -42,6 +45,25 @@ export default class DocenteNuevoProyecto extends Component {
                 alert('error en la materia');
             });
     }
+
+    getMethodologies = async () => {
+        await axios.get(getMethodologiesUrl, {
+            headers: {
+                'Authorization': cookies.get('token')
+            }
+        })
+            .then(response => {
+                console.log("AAAAAAAAA");
+                console.log(response.data);
+                console.log("AAAAAAAAA");
+                const methodologies = response.data.map(methodology => ({ name: methodology.name, id: methodology.id }));
+                this.setState({ methodologies });
+            })
+            .catch(error => {
+                console.log(error);
+                alert('error en las metodologias');
+            });
+    }
     
 
     handleChange = async e => {   //con este metodo guardamos en el estado el valor del imput de acuerdo 
@@ -54,30 +76,27 @@ export default class DocenteNuevoProyecto extends Component {
     }
 
     render() {
-        var name;
-        var methodologyId;
-        var teacherId;
+        window.onload = this.getMethodologies;
 
         return (
             <div className="containerPrincipal">
                 <div className="containerSecundario">
                     <input
-                        type="user"
+                        type="name"
                         className="inputs"
-                        name="username"
-                        placeholder="Usuario"
+                        name="name"
+                        placeholder="Nombre del Proyecto"
                         maxLength="10"
                         onChange={this.handleChange} />
                     <br />
-                    <input
-                        type="password"
-                        className="inputs"
-                        name="password"
-                        placeholder="Contraseña"
-                        maxLength="10"
-                        onChange={this.handleChange} />
-                    <br />
-                    <button onClick={() => this.newProject(name, methodologyId, teacherId)} className="submit-button">{"Crear"}</button>
+                    <div className="classcontainer">
+                            {this.state.methodologies.map(methodology => { 
+                                return (<button className="classButton" id={methodology.id} onClick={() => this.setState(state => state.methodologyId = methodology.id)}>
+                                    {methodology.name}
+                                    </button>) 
+                                })};
+                    </div>
+                    <button onClick={() => this.newProject(this.state.name, this.state.methodologyId, this.state.teacherId)} className="submit-button">{"Crear"}</button>
                 </div>
             </div>
         );
