b286d4b341deb726139ddbf67accc7115397c133
Marigaitan <marielgaitan93@gmail.com>
2021-08-23 22:42:17 -0300

    segwe

--- proyecto/src/pages/AlumnoClassroom.js
+++ proyecto/src/pages/AlumnoClassroom.js
@@ -3,11 +3,28 @@ import Cookies from 'universal-cookie/es6';
 import axios from 'axios';
 
 const cookies = new Cookies();
+let classparamUrl = "http://localhost:8080/classrooms/" + cookies.get('classid');
 
 export default class AlumnoClassroom extends Component {
+    classParam= async () => {
+        await axios.get(classparamUrl , {
+            headers: {
+              'Authorization': cookies.get('token')
+            }
+          }) 
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+                alert('error en la materia');
+            })
     
+
+    }
     render() {   
         console.log(cookies.get('classid'));
+        window.onload = this.classParam;
         return (
             
             <div>
