622da34e7db65368f803aa1a0eb29b2f4d55c757
Marigaitan <marielgaitan93@gmail.com>
2021-09-21 15:38:46 -0300

    tuf6ur6r

--- proyecto/src/pages/MenuAdmin.js
+++ proyecto/src/pages/MenuAdmin.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import Cookies from 'universal-cookie/es6';
-import img from '../Images/account.png';
+import '../css/Global.css';
 import '../css/MenuAdmin.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HeaderAdmin from "./HeaderAdmin";
 
 const cookies = new Cookies();
 
 export default class MenuAdmin extends Component {
-    cerrarSesion=()=>{
-        cookies.remove('token', {path: "/"});
-        cookies.remove('username', {path: "/"});
-        cookies.remove('role', {path: "/"});
-        cookies.remove('id', { path: "/" });
-        this.props.history.push('/'); //lo redirijo al login
-    }
 
     componentDidMount(){    //para que lo redirija al login si no hay token
         if(!cookies.get('token')|| cookies.get('role') !== "ROLE_ADMIN"){
@@ -26,21 +20,11 @@ export default class MenuAdmin extends Component {
         console.log('username: ' + cookies.get('username'));
         return (
             <div className="mainContainer">
+                <HeaderAdmin/>
                 <div className="secContainer">
-                <div className="barraUser">
-                        <img  src={img} alt="No se encuentra la imagen" id="logoAccount"/>
-                        <div className="menuContent">
-                                <a onClick={()=>{this.irPerfil()}}>Ver Perfil</a>
-                                <a onClick={() => this.cerrarSesion()}>Cerrar sesión</a>
-                            </div>
-                            <h1 id="userName">{cookies.get('username')}</h1>
-                        <div>
-                        
-                        <button className="classButton" id="12" onClick={() => this.goDocenteAbm()}>{"Docentes"} </button>
-                        <button className="classButton" id="12" onClick={() => this.goAlumnosAbm()}>{"Alumnos"} </button>
-                        <button className="classButton"  id="12" onClick={() => this.goCursosAbm()}>{"Cursos"} </button>
-                        </div>
-                    </div>
+                    <button className="classButton" id="12" onClick={() => this.goDocenteAbm()}>{"Docentes"} </button>
+                    <button className="classButton" id="12" onClick={() => this.goAlumnosAbm()}>{"Alumnos"} </button>
+                    <button className="classButton"  id="12" onClick={() => this.goCursosAbm()}>{"Cursos"} </button>  
                 </div>
             </div>
             
