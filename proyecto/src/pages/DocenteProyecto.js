e3dd9be53f50ebedc0a456c33550d5a4e4745b04
Marigaitan <marielgaitan93@gmail.com>
2021-09-21 15:02:28 -0300

    tyfk

--- proyecto/src/pages/DocenteProyecto.js
+++ proyecto/src/pages/DocenteProyecto.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react'
 import Cookies from 'universal-cookie/es6';
+import '../css/Global.css';
 
 import {API_HOST} from "../constants";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import HeaderTeacher from "./Header"
 
 const cookies = new Cookies();
-let getActivitiessUrl = API_HOST + "classroom/" + cookies.get('classid') + "/projects"+ cookies.get('projectid') + "/activities";
+let getActivitiessUrl = API_HOST + "classroom/" + cookies.get('classid') + "/projects"+ cookies.get('projectid') + "/lessons";
 
 export default class DocenteProyecto extends Component {
     
     render() {
         return (
-            <div>
-                <HeaderTeacher /> 
-                Vista del Proyecto para el Docente
-                <br/>
-                <Button color="primary" size="lg">Crear Clase</Button>{' '}
+            <div className="mainContainer">
+                <HeaderTeacher />
+                <div className="secContainer">
+                    Vista del Proyecto para el Docente
+                    <br/>
+                    <Button color="primary" size="lg">Crear Clase</Button>{' '}
+                </div>
             </div>
         )
     }
