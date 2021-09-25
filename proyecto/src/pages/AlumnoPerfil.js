54ae211b70d6f5b3f0ffd4ddcf4cf0853023bafc
Nazareno <naza@frba.utn.edu.ar>
2021-09-25 19:52:45 -0300

    CSS BOOTSTRAP para informacion de perfil

--- proyecto/src/pages/AlumnoPerfil.js
+++ proyecto/src/pages/AlumnoPerfil.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Cookies from 'universal-cookie/es6';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/MenuAlumno.css';
+import '../css/PerfilAlumno.css';
 import axios from 'axios';
 import HeaderStudentPerfil from "./HeaderAlumnoPerfil";
 import {API_HOST} from "../constants";
@@ -9,9 +10,9 @@ import {API_HOST} from "../constants";
 const AlumnoPerfil = () => {
 
         return (
-            <div className="mainContainer">
+            <div className="main">
                 <HeaderStudentPerfil/>
-                <div className="secContainer">
+                <div className="secondary col-md-4">
                     <h1>Perfil</h1>
                     <br />                
                 </div>
