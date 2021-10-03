20981be9bbdca73072ff5ea3765bd4370b4e42c4
Marigaitan <marielgaitan93@gmail.com>
2021-10-03 13:47:07 -0300

    fwfw

--- proyecto/src/pages/NuevaClase.js
+++ proyecto/src/pages/NuevaClase.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ButtonGroup ,Button, Label, Form, FormGroup, Input, FormText, Col } from 'reactstrap'
+import { ButtonGroup, Button, Label, Form, FormGroup, Input, FormText, Col } from 'reactstrap'
 import HeaderTeacher from './Header'
 import Cookies from 'universal-cookie/es6';
 import '../css/Global.css';
@@ -8,30 +8,83 @@ import { API_HOST } from "../constants";
 
 const cookies = new Cookies();
 
+
+
 export default class NuevaClase extends Component {
+    constructor(props) {        //constructor de mi clase
+        super(props);
+        this.state = {form: { Clasename: '', consigna: '', startDate: '', startTime: '', dueDate: '', dueTime: '' }
+        };
+    }
+
+    handleChange = async e => {   //con este metodo guardamos en el estado el valor del input
+        console.log(e.target.value);
+
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+
+            }
+        })
+    }
+
+    newClase() {
+        let newClaseUrl = API_HOST + "lesson";
+        axios.post(newClaseUrl,
+            {
+                id: "0",
+                position: "0",
+                name: "string",
+                projectId: cookies.get('projectid'),
+                dueDate: "0",
+                startDate: "0",
+                active: "false"
+            },
+            {
+                headers: {
+                    'Authorization': cookies.get('token')
+                }
+            }
+        )
+            .then(response => {
+                console.log(response);
+                cookies.set('claseid', response.data, { path: "/menudocente/classroom" });
+                window.location.href = "/menudocente/classroom/proyecto" + cookies.get('projectid');
+            })
+            .catch(error => {
+                console.log(error);
+                alert('No se pudo crear la Clase. Verifique que todos los campos esten completos')
+            }).then(this.goProject());
+    }
+    goProject() {
+        window.location.href = "/menudocente/classroom" + cookies.get('projectid');
+    }
+
     render() {
         return (
             <div className="mainContainer">
                 <HeaderTeacher />
                 <div className='formProyectoClase'>
                     <h2>Clase Nueva</h2>
-                    <div className="boxEnunciado">
-                        <FormGroup>
-                            <Label for="exampleText"><h4>Ingrese el enunciado de la clase que quiere compartir
-                                con los alumnos:</h4></Label>
-                            <Input type="textarea" name="text" id="exampleText" />
-                        </FormGroup>
-                    </div>
-                    <div className="boxMaterial">
-                        <FormGroup>
-                            <Label><h4>Agregar material de soporte</h4></Label>
-                            <Input type="file" name="file" id="exampleFile" />
-                            <FormText color="muted">
-                                En esta sección, puede adjuntar material de soporte que quiera dejar disponible al alumno al ingresar a esta clase.
-                            </FormText>
-                        </FormGroup>
-                    </div>
-                    {/* <div className="boxFechas">
+                    <Form>
+                        <div className="boxEnunciado">
+                            <FormGroup>
+                                <Label for="exampleText"><h4>Ingrese el enunciado de la clase que quiere compartir
+                                    con los alumnos:</h4></Label>
+                                <Input type="textarea" name="text" id="exampleText" />
+                            </FormGroup>
+                        </div>
+                        <div className="boxMaterial">
+                            <FormGroup>
+                                <Label><h4>Agregar material de soporte</h4></Label>
+                                <Input type="file" name="file" id="exampleFile" />
+                                <FormText color="muted">
+                                    En esta sección, puede adjuntar material de soporte que quiera dejar disponible al alumno al ingresar a esta clase.
+                                </FormText>
+                            </FormGroup>
+                        </div>
+                        {/* <div className="boxFechas">
                         <Form inline>
                             <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
 
@@ -77,20 +130,21 @@ export default class NuevaClase extends Component {
                             </FormGroup>
                         </Form>                        
                     </div>*/}
-                    <div className="boxActividades">
-                        <ButtonGroup vertical>
-                            <Button outline color="secondary">Agregar Cuestionario</Button>
-                            <Button outline color="secondary">Agregar Selección Múltiple</Button>
-                            <Button outline color="secondary">Cargar Video</Button>
-                        </ButtonGroup>
-                    </div><br/>
-                    <div className="boxButtons">
-                        <FormText color="muted">
-                            Más adelante podrá volver a la clase creada y editarla!
-                        </FormText><br/>
-                        <Button color="primary">Crear</Button>{' '}
-                        <Button color="secondary">Cancelar</Button>
-                    </div>
+                        <div className="boxActividades">
+                            <ButtonGroup vertical>
+                                <Button outline color="secondary">Agregar Cuestionario</Button>
+                                <Button outline color="secondary">Agregar Selección Múltiple</Button>
+                                <Button outline color="secondary">Cargar Video</Button>
+                            </ButtonGroup>
+                        </div><br />
+                        <div className="boxButtons">
+                            <FormText color="muted">
+                                Más adelante podrá volver a la clase creada y editarla!
+                            </FormText><br />
+                            <Button color="primary" size="lg">Crear</Button>{' '}
+                            <Button color="secondary" size="lg">Cancelar</Button>
+                        </div>
+                    </Form>
                 </div>
             </div>
         )
