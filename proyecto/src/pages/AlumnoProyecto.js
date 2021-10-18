4b8aa3b1d46472b06b65910f658b84632315537b
Marigaitan <marielgaitan93@gmail.com>
2021-10-17 21:05:53 -0300

    clases

--- proyecto/src/pages/AlumnoProyecto.js
+++ proyecto/src/pages/AlumnoProyecto.js
@@ -1,19 +1,117 @@
 import React, { Component } from 'react'
 import Cookies from 'universal-cookie/es6';
 import '../css/Global.css';
-
-import {API_HOST} from "../constants";
+import '../css/AlumnoProyecto.css';
+import LogoMini from '../Images/logoMini.png';
+import axios from 'axios';
+import { API_HOST } from "../constants";
 import HeaderStudent from "./HeaderAlumno"
+import { VerticalTimeline } from 'react-vertical-timeline-component';
+import VerticalTimelineElement from './VerticalTimeLineElement';
+import PropTypes from 'prop-types';
 
 const cookies = new Cookies();
 
+VerticalTimeline.PropTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]).isRequired,
+    className: PropTypes.string,
+    animate: PropTypes.bool,
+    layout: PropTypes.oneOf([
+        '1-column-left',
+        '1-column',
+        '2-columns',
+        '1-column-right',
+    ]),
+};
+
+VerticalTimeline.defaultProps = {
+    animate: true,
+    className: '',
+    layout: '2-columns',
+};
+
 export default class AlumnoProyecto extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            grupo: '', integrantes:[], lessons: [], project: ''
+        };
+    }
+
+    async componentDidMount() {
+
+        let getProjectDetailsUrl = API_HOST + "project/" + cookies.get('projectid')
+        //let getGrupoUrl = API_HOST +"project/" + cookies.get('projectid') + "/groups/student/" + cookies.get('id');
+        //let getIntegrantes = API_HOST + "group/" + grupo.id + "students";
+        let getLessonsUrl = API_HOST + "project/" + cookies.get('projectid') + "/lessons";
+
+        //AXIOS
+        const requestOne = axios.get(getProjectDetailsUrl, { headers: { 'Authorization': cookies.get('token') } });
+        //const requestTwo = axios.get(getGrupoUrl, { headers: { 'Authorization': cookies.get('token') } });
+        //const requestThree = axios.get(getIntegrantes, {headers: { 'Authorization': cookies.get('token') } });
+        const requestFour = axios.get(getLessonsUrl, { headers: { 'Authorization': cookies.get('token') } });
+
+        await axios.all([requestOne
+            //, requestTwo, requestThree
+            ,requestFour
+        ])
+            .then(axios.spread((project
+                //, grupo, integrantes
+                , lessons
+            ) => {
+                console.log(project.data,
+                     //grupo.data,
+                     //integrantes.data,
+                     lessons.data
+                );
+
+                this.setState({
+                    project: project.data,
+                    //grupo: grupo.data,
+                    //integrantes: integrantes.data,
+                    lessons: lessons.data
+                })
+            }))
+            .catch(error => {
+                console.log(error)
+            });
+    }
+
     render() {
         return (
             <div className="mainContainer">
                 <HeaderStudent />
-                <div className="secContainer">
-                    Vista del Proyecto para el Alumno
+                <div className="AlumnoProyecto">
+                    <div>
+                        <h2>{this.state.project.name}</h2>
+                    </div>
+                    <div className="myProject">
+                        <div className="availableLessons">
+                            <h2>Clases</h2>
+                            <VerticalTimeline>
+                                <VerticalTimelineElement
+                                    className="vertical-timeline-element--work"
+                                    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                                    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                                    date="2011 - present"
+                                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                                    img={LogoMini}
+                                >
+                                    <h3>Test</h3>
+                                </VerticalTimelineElement>
+                            </VerticalTimeline>
+                        </div>
+                        <div className="myTeam">
+                            <h2>Mi Equipo</h2>
+                            <div>
+                                {/* {this.state.integrantes.map(grupo => { return (<div key={integrantes.id} id={integrantes.id}><h3><li>{grupo}</li></h3></div>) })} */}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
