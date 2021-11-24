c05cf0655c2596cc650f66734149823143c5ed8e
Javier Leonardo Soto <jlsoto85@gmail.com>
2021-11-24 12:46:11 -0300

    Implementacion inicial de instant feedback

--- proyecto/src/pages/HeaderAlumno.js
+++ proyecto/src/pages/HeaderAlumno.js
@@ -3,6 +3,16 @@ import React, { useState } from "react";
 import cerrarSesion from "./logout";
 import Cookies from "universal-cookie/es6";
 import { GiRaceCar } from "react-icons/gi";
+import { API_HOST } from "../constants";
+import {
+  Table,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from "reactstrap";
+import { useIntervalWhen } from "rooks";
 
 //import react-pro-sidebar components
 import {
@@ -25,11 +35,17 @@ import { Link } from "react-router-dom";
 //import sidebar css from react-pro-sidebar module and our custom css
 import "react-pro-sidebar/dist/css/styles.css";
 import "../css/Header.css";
+import axios from "axios";
 
 const cookies = new Cookies();
 const HeaderStudent = () => {
   //create initial menuCollapse state using useState hook
   const [menuCollapse, setMenuCollapse] = useState(false);
+  const [firstRender, setFirstRender] = useState(true);
+  const [rewards, setRewards] = useState([]);
+  const [newRewards, setNewRewards] = useState([]);
+  const [openNewRewardsModal, setOpenNewRewardsModal] = useState(false);
+  
 
   //create a custom function that will change menucollapse state from false to true and true to false
   const menuIconClick = () => {
@@ -37,8 +53,77 @@ const HeaderStudent = () => {
     menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
   };
 
+
+  useIntervalWhen(() => {
+    getRecentAchievements();
+    },
+    5000,
+    true,
+    true
+  );
+
+
+  const getRecentAchievements = async () => {
+    axios.defaults.headers.common["Authorization"] = cookies.get("token");
+    axios.defaults.baseURL = API_HOST;
+
+
+
+    if (firstRender) {
+      let currentRewards = (await axios.get("user/" + cookies.get("id") + "/rewards")).data;
+      setRewards(rewards => rewards = currentRewards);
+      setFirstRender(firstRender => firstRender = false);    
+    }
+    else {
+      let currentRewards = (await axios.get("user/" + cookies.get("id") + "/rewards")).data;
+      let diff = currentRewards.filter(x => !rewards.some(y => y.id == x.id));
+
+      if (diff.length > 0) {
+        setRewards(rewards => rewards = currentRewards);
+        setNewRewards(newRewards => newRewards = diff);
+        setOpenNewRewardsModal(open => open = true);
+      }
+    }
+
+  }
+
+
+  const newRewardsModal = () => {
+      return (
+        <Modal isOpen={openNewRewardsModal} className="modalStyle">
+          <ModalHeader size="lg">{"Felicitaciones!"}</ModalHeader>
+          <ModalBody>
+            {newRewards.map(reward => {
+              return (
+                <div className="card ms-3 mt-2" style={{ maxWidth: 120 }}>
+                <div className="row no-gutters">
+                  <div className="d-flex flex-column align-items-center text-center">
+                    <img
+                      src={`./medallas_cursos/${reward.imageData}.png`}
+                      alt={reward.id}
+                      width="100"
+                    />
+                  </div>
+                  <div>
+                    <div>
+                      <p style={{ color: "red", fontWeight: "bold" }}>{reward.name}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              )
+            })}
+          </ModalBody>
+          <ModalFooter className="modalFooter">
+          <Button color="primary" onClick={() => setOpenNewRewardsModal(firstRender => firstRender = false)}> Ok </Button>
+          </ModalFooter>
+        </Modal>
+      )
+  }
+
   return (
     <div id="header">
+      {newRewardsModal()}
       {/* collapsed props to change menu size using menucollapse state */}
       <ProSidebar collapsed={menuCollapse}>
         <SidebarHeader>
