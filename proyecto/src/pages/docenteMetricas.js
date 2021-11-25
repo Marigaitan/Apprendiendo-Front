ccaeb1749e0a9b91893e3678dcc3ea887a212777
Nazareno <naza@frba.utn.edu.ar>
2021-11-24 21:07:28 -0300

    Metric 2

--- proyecto/src/pages/docenteMetricas.js
+++ proyecto/src/pages/docenteMetricas.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Cookies from "universal-cookie/es6";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import HeaderStudent from "./HeaderAlumno";
+import { API_HOST } from "../constants";
+import "../css/Global.css";
+import "../css/MenuDocente.css";
+import HeaderTeacher from "./Header";
+import Background from "../Images/fondoLetras.png";
+import { Button } from "reactstrap";
 
+const cookies = new Cookies();
 export const docenteMetricas = () => {
-  return <div>Hola Metricas</div>;
+  return (
+    <div className="mainContainer">
+      <HeaderTeacher />
+      <div className="secContainer">
+        <div className="misCursos">
+          <h1 id="Title">
+            <b>Metricas</b>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
